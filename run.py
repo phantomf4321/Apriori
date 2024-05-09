e42e5787de663ef8177cd54738363237cd11c873
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-09 09:46:59 +0330

    Find columns where all rows are not zero

--- run.py
+++ run.py
@@ -1,39 +1,24 @@
 from apriori import GROUP
 from engine import Engine
 
-import pandas as pd
-import numpy as np
 
 
-def calculate_similarity(group):
-    members = group.index
-    ratings = group.to_numpy()  # Convert DataFrame to a NumPy array
 
-    # Calculate the Pearson correlation coefficient similarity
-    curr_dataframe = np.corrcoef(ratings, rowvar=True)
 
-    # Convert the matrix to a DataFrame with proper index and columns
-    final_dataframe = pd.DataFrame(curr_dataframe, index=members, columns=members)
 
-    return final_dataframe
+G1 = GROUP('dataset/raiting.csv', 5)
+Group = G1.set_matrix()
+print(Group)
 
-# Create a dictionary with ten columns
-data = {
-    'I1': np.array([0, 3, 4, 3, 0]),
-    'I2': np.array([2, 2, 0, 4, 2]),
-    'I3': np.array([4, 0, 3, 0, 0]),
-    'I4': np.array([3, 4, 5, 5, 3]),
-    'I5': np.array([0, 0, 3, 2, 0]),
-    'I6': np.array([5, 4, 4, 4, 5]),
-    'I7': np.array([0, 2, 3, 0, 4]),
 
-}
+# Find columns where all rows are not zero
+non_zero_cols = Group.loc[:, (Group != 0).any(axis=0)].columns
 
-# Create the DataFrame
-df = pd.DataFrame(data)
+# Get the first 10 columns
+first_10_cols = non_zero_cols[:10]
 
-print(df)
-print(calculate_similarity(df))
+print(first_10_cols)
+
 
 """
 G1 = GROUP('dataset/raiting.csv', 5)

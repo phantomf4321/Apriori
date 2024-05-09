2ee6a6820d08160d3805afa537f10f6ac32cf227
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-09 09:57:21 +0330

    columns_to_keep = [1, 2, 3, 5, 6, 7, 8, 345, 370, 502]

--- run.py
+++ run.py
@@ -5,14 +5,19 @@ from engine import Engine
 
 
 
-
 G1 = GROUP('dataset/raiting.csv', 5)
 Group = G1.set_matrix()
 print(Group)
 
 # Assuming df is your DataFrame
-columns_to_keep = [1, 2, 3, 5, 6, 7, 8, 10, 11, 12]
+columns_to_keep = [1, 2, 3, 5, 6, 7, 8, 345, 370, 502]
 new_df = Group[columns_to_keep]
+
+print(new_df)
+
+
+
+
 
 """
 G1 = GROUP('dataset/raiting.csv', 5)

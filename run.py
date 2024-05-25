6dca1fb239217723901d09a33664ebc3fc803b6e
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-25 20:46:00 +0330

    top-k

--- run.py
+++ run.py
@@ -144,6 +144,8 @@ leader = identify_leader(trust, similarity, centerality, len(df))
 
 print("Dataframe\n", df)
 
+for i in df.index:
+    print(i)
 
 NG = len(df)
 LIFNg = (calculate_similarity(df) + calculate_trust(df) + calculate_centerality(df))/3*(NG-1)
